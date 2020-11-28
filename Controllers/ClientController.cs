aecb3ab98a7d98e8124f2eef165c9258bb9ca1dd
vitormmh <74314572+vitormmh@users.noreply.github.com>
2020-11-28 16:26:54 +0000

    primeiro commit

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebComplete.Models;
+using System.Data.Entity;
+
+namespace WebComplete.Controllers
+{
+    public class ClientController : Controller
+    {
+
+        ApplicationDbContext _context;
+
+        public ClientController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult Index()
+        {
+
+            List<Client>Clientes = _context.Client.Include(c => c.Plan).ToList();
+
+            return View(Clientes);
+        }
+
+        public ActionResult New()
+        {
+            List<Plan> plans = new List<Plan>();
+            plans =_
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModel/PlanUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebComplete.Models.ViewModel
+{
+    public class PlanUserViewModel
+    {
+        public Client Utilizador { get; set; }
+        public List<Plan> Planos { get; set; }
+        public List<Plan2> PlanoA { get; set; }
+    }
+}
